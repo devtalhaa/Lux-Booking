fa0c6aead415d1ba6284392df7898766de1c5816
devtalhaa <dev.talhaa@gmail.com>
2026-02-07 02:16:53 +0500

    Removed Extra Comments

--- src/components/layout/header/index.tsx
+++ src/components/layout/header/index.tsx
@@ -22,10 +22,10 @@ const Header = () => {
     <nav className="flex flex-col custom-md:px-0 px-[20px] py-[24px] lg:max-w-[960px] custom-md:max-w-[746px] mx-auto">
 
       <div className="flex items-center justify-between">
-        {/* Logo */}
+
         <img src="/assets/mainLogo.svg" alt="Lux Logo" className="w-[67px] h-[36px] custom-md:w-[60px] custom-md:h-[32px]" />
 
-        {/* Desktop Navigation */}
+
         <div className="hidden custom-md:flex items-center gap-[8px]">
           <div className="relative rounded-[8px] max-h-[33px] [letter-spacing:-.01em] py-2 px-2 font-body bg-[#F7F7F7] inline-flex items-center leading-[18px] text-black font-medium cursor-pointer">
             <Globe className="w-[16.25px] h-[16.25px] mr-[5px] mt-[1px]" strokeWidth="1.6" />
@@ -49,7 +49,7 @@ const Header = () => {
           </Button>
         </div>
 
-        {/* Mobile Navigation */}
+
         <div className="flex custom-md:hidden items-center gap-[10px]">
           <div className="relative rounded-lg bg-[#F7F7F7] h-[33px] flex items-center justify-center px-2 text-[14px] text-black cursor-pointer">
             <Globe className="mr-[6.5px] w-4 h-4 mt-[1px]" strokeWidth={1.6} />
@@ -77,7 +77,7 @@ const Header = () => {
         </div>
       </div>
 
-      {/* Mobile Menu Panel */}
+
       {mobileMenuOpen && (
         <div className="flex flex-col gap-[8px] mt-8 custom-md:hidden">
           <Link href="/login" className="w-full">
